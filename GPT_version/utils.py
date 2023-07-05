86d9a77e3f2d019d2e5d0b67b85f5e9a74b43e42
Enoch <enoch.kung@uk.ey.com>
2023-07-05 11:57:59 +0100

    Extracting used functions function

--- GPT_version/utils.py
+++ GPT_version/utils.py
@@ -1,5 +1,7 @@
-import re
+import ast
+import builtins
 import os
+import re
 import sys
 
 
@@ -32,3 +34,46 @@ def extract_imports(script_path):
                 imports.append(module_dir)
 
     return imports
+
+
+def extract_used_imported_functions(script_path):
+    used_functions = []
+    imported_functions = {}
+
+    # Parse the script's abstract syntax tree (AST)
+    with open(script_path, 'r') as file:
+        script_content = file.read()
+        tree = ast.parse(script_content, script_path)
+
+        # Extract the imported functions and their aliases
+        for node in ast.iter_child_nodes(tree):
+            if isinstance(node, ast.ImportFrom):
+                module_name = node.module
+                for alias in node.names:
+                    imported_functions[f"{module_name}.{alias.name}"] = alias.asname or alias.name
+            elif isinstance(node, ast.Import):
+                for alias in node.names:
+                    imported_functions[alias.name] = alias.asname or alias.name
+
+        # Extract the names of used functions
+        for node in ast.walk(tree):
+            if isinstance(node, ast.Call) and isinstance(node.func, ast.Name):
+                function_name = node.func.id
+                if function_name not in builtins.__dict__:
+                    used_functions.append(function_name)
+
+    # Filter out the unused imported functions
+    used_imported_functions = {
+        function_name: function_alias
+        for function_name, function_alias in imported_functions.items()
+        if function_alias in used_functions
+    }
+
+    return used_imported_functions
+
+
+if __name__ == '__main__':
+    # Example usage
+    script_path = r'C:\Users\FW246CA\repos\mneme\mneme\test_folder\main.py'
+    used_imported_functions = extract_used_imported_functions(script_path)
+    print(used_imported_functions)
